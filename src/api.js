f6e56b10b35b5910d1f47804fa801d568db43c17
schokett <skrollanelenazimmermann@gmail.com>
2026-05-29 13:44:44 +0200

    get Joke function

---
+++ src/api.js
@@ -0,0 +1,28 @@
+const newJokeButton = document.querySelector(".section-joke__button");
+const jokeField = document.querySelector(".section-joke__joke");
+
+//Get api joke
+newJokeButton.addEventListener("click", getJoke);
+
+async function getJoke() {
+  fetch("https://witzapi.de/api/joke/", {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      replaceJoke(data);
+    })
+    .catch((error) => {
+      console.error("Fehler:", error);
+    });
+}
+
+//replace a joke
+async function replaceJoke(dataJoke) {
+  if (jokeField) {
+    const cleanJoke = dataJoke[0].text;
+    const manipulatedJoke = cleanJoke.slice(0, -1);
+    jokeField.textContent = manipulatedJoke;
+  }
+}
