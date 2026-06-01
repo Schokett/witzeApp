3b10c2dab34a8a24b7493e3411b2f39c5dad5e19
schokett <skrollanelenazimmermann@gmail.com>
2026-06-01 11:15:40 +0200

    forbidden same to save in localstorage

--- src/saveJoke.js
+++ src/saveJoke.js
@@ -6,8 +6,10 @@ saveJokeButton.addEventListener("click", catchJoke);
 function catchJoke() {
   const currentJoke = document.querySelector(".section-joke__joke").textContent;
 
-  savedJokes.push(currentJoke);
-  saveJoke();
+  if (currentJoke && !savedJokes.includes(currentJoke)) {
+    savedJokes.push(currentJoke);
+    saveJoke();
+  }
 }
 function saveJoke() {
   localStorage.setItem("savedJokes", JSON.stringify(savedJokes));
